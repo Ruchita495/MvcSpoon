045b76d170a3a16f12dfc3ed9ed9961bb428eb2c
Ruchita495 <122808258+Ruchita495@users.noreply.github.com>
2023-10-12 17:32:23 -0400

    add validation critarea

--- Models/Spoons.cs
+++ Models/Spoons.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,10 +11,26 @@ namespace MvcSpoon.Models
     {
         // Add a field with datatype
         public int Id { get; set; }
+        // Add validation critarea 
+        [StringLength(30, MinimumLength = 3)]
+        [Required]
         public string Type { get; set; }
+
+        [StringLength(10, MinimumLength = 2)]
+        [Required]
         public string Size { get; set; }
+
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
+
+        [StringLength(15, MinimumLength = 3)]
+        [Required]
         public string Material { get; set; }
+
+        [StringLength(15, MinimumLength = 5)]
+        [Required]
         public string Use { get; set; }
         public int Rating { get; set; }
     }
